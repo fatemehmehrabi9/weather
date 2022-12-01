30573b5c449dacf7bca2b0f3930f77ea902d1ea8
fatemehmehrabi9 <111909410+fatemehmehrabi9@users.noreply.github.com>
2022-12-01 13:14:18 +0330

    Add files via upload

---
+++ fatemehmehrabiw/fatemeh/js/api.js
@@ -0,0 +1,32 @@
+
+		const app = document.querySelector("#app");
+		let btnAdd = document.querySelector(".add")
+
+		btnAdd.onclick = function () {
+
+			let cityName = document.querySelector(".input").value;
+
+			fetch(`http://api.weatherapi.com/v1/current.json?key=b387aea5193e49c59a8142534221711&q=${cityName}&aqi=no`)
+				.then(res => res.json())
+				.then(data =>
+					app.innerHTML = `
+		<div class="weather w-50 mx-auto my-3 ">
+			<div class="card text-bg-warning mb-3" style="width: 300px; height: 300px; text-align: center;">
+            <div class="card-header"  style="background-color: darkorange; width: 300px; height: 300px; ">
+				<img src="${data.current.condition.icon}" class="weather-icon" alt="" style=" margin-top: 25%;  width: 100px; height: 100px;">
+			</div>
+              <div class="card-body">
+              <h5 class="card-title">
+				<p class="city">${data.location.name}</p>
+			  </h5>
+              <p class="card-text">
+				<p class="deg">${data.current.temp_c} °C</p>
+
+			  </p>
+          </div>
+       </div>
+
+        </div>
+		`
+				)
+		}
